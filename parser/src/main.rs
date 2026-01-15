2a4af3ae31787de2fab00e9cfd7263bfc6d54a72
Leo Libros <leonardinoeb@gmail.com>
2026-01-15 09:43:38 -0600

    WHY DO I NOT COMMIT UNTIL IVE DONE 20% OF THE PROJECT

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -9,28 +9,37 @@
 
 use std::collections::HashMap;
 
+#[derive(Debug, Clone, PartialEq)]
+enum TokenType {
+  // Keywords
+  Declare,
+  If,
+  Print,
+
+  // Literals
+  Identifier,
+  Number,
+
+  // Operations
+  Plus,
+  Minus,
+  Equals,
+  Star,
+  Slash,
+
+  // Misc
+  LeftParen,
+  RightParen,
+  Semicolon,
+  EOF,
+}
+
 fn main() {
-  enum TokenType {
-    // Keywords
-    Declare,
-    If,
-    Print,
 
-    // Literals
-    Identifier,
-    Number,
-
-    // Operations
-    Plus,
-    Minus,
-    Equals,
-    Star,
-    Slash,
-
-    // Misc
-    LeftParen,
-    RightParen,
-    EOF,
+  #[derive(Debug)]
+  struct Token {
+    token_type: TokenType,
+    literal: String,
   }
 
   // Gonna need a hashmap for them keywords
@@ -50,6 +59,7 @@ fn main() {
   }
 
   impl <'a> Lexer<'a> {
+    //* Self::new() */
     fn new(input: &'a str) -> Self {
       let mut lexer = Lexer {
         input,
@@ -61,6 +71,8 @@ fn main() {
       lexer.read_char();
       lexer
     }
+
+    //* Read character */
     fn read_char(&mut self) {
       if self.read_position >= self.input.len() {
         self.ch = None;
@@ -76,6 +88,124 @@ fn main() {
         self.read_position += 1;
       }
     }
+
+    //* Helper func: Identifiers */
+    fn read_identifier(&mut self) -> String {
+      let start = self.pos;
+      while let Some(c) = self.ch {
+        if is_letter(c) {
+          self.read_char();
+        } else {
+          break;
+        }
+      }
+      self.input[start..self.pos].to_string()
+    }
+
+    //* Helper func: Numbers */
+    fn read_number(&mut self) -> String {
+      let start = self.pos;
+      while let Some(c) = self.ch {
+        if c.is_ascii_digit() {
+          self.read_char();
+        } else {
+          break;
+        }
+      }
+      self.input[start..self.pos].to_string()
+    }
+
+    //* Match characters */
+    fn next_token(&mut self, keywords: &HashMap<&str, TokenType>) -> Token {
+      
+      // Like skio if it's not good enuff
+      while matches!(self.ch, Some(' ' | '\n' | '\t' | '\r')) {
+        self.read_char();
+      }
+
+      let tok: Token = match self.ch {
+        //* Case = */
+        Some('=') => Token {
+          token_type: TokenType::Equals,
+          literal: "=".to_string(),
+        },
+
+        //* Case + */
+        Some('+') => Token {
+          token_type: TokenType::Plus,
+          literal: "+".to_string(),
+        },
+
+        //* Case - */
+        Some('-') => Token {
+          token_type: TokenType::Minus,
+          literal: "-".to_string(),
+        },
+
+        //* Case * */
+        Some('*') => Token {
+          token_type: TokenType::Star,
+          literal: "*".to_string(),
+        },
+
+        //* Case / */
+        Some('/') => Token {
+          token_type: TokenType::Slash,
+          literal: "/".to_string(),
+        },
+
+        //* Case ; */
+        Some(';') => Token {
+          token_type: TokenType::Semicolon,
+          literal: ";".to_string(),
+        },
+
+        //* Case EOF */
+        None => Token {
+          token_type: TokenType::EOF,
+          literal: "".to_string(),
+        },
+
+        //* Case Identifier */
+        Some(c) if is_letter(c) => {
+          let ident = self.read_identifier();
+          let token_type = keywords
+            .get(ident.as_str())
+            .cloned()
+            .unwrap_or(TokenType::Identifier);
+
+          return Token {
+            token_type,
+            literal: ident,
+          };
+        },
+
+        //* Case Number */
+        Some(c) if c.is_ascii_digit() => {
+          let num = self.read_number();
+          return Token {
+            token_type: TokenType::Number,
+            literal: num,
+          }
+        },
+
+        _ => {
+          // Unknown character
+          Token {
+            token_type: TokenType::EOF,
+            literal: "".to_string(),
+          }
+        }
+      };
+
+      self.read_char();
+      tok
+    }
+  }
+
+  //* Helper func: is_letter() */
+  fn is_letter(c: char) -> bool {
+    c.is_alphabetic() || c == '_'
   }
 
   let my_code: &str = "let a = 5;";
@@ -86,6 +216,13 @@ fn main() {
 
 
 
-  // STARTING THE LEXER FUNCS FINALLY I THINK
+  // START TESTING THE LEXER FUNCS FINALLY I THINK
+  loop {
+    let tok = lexer.next_token(&keywords);
+    println!("{:?}", tok);
 
+    if tok.token_type == TokenType::EOF {
+      break;
+    }
+  }
 }
